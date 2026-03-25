05018357a7cf5d2def7b48a4e211786979da17d1
Krebv <118159046+Krebv@users.noreply.github.com>
2026-03-25 16:02:08 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wbgt-heat-stress-monitor/',
+  base: '/wbgt-heat-stress-monitor/',  // Your repo name with slashes
 })
