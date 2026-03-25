aaccfc087aab27ccc8650dd9d646aecb373fa470
Krebv <118159046+Krebv@users.noreply.github.com>
2026-03-25 15:35:52 +0800

    Fix formatting in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/wbgt-heat-stress-monitor/', // Repository name
-  build: {
-    outDir: 'dist',
-  },
+  base: '/wbgt-heat-stress-monitor/',
 })
