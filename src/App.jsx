f581846522e8de4aa101171a87f969ddaf6450a8
Krebv <118159046+Krebv@users.noreply.github.com>
2026-03-26 10:41:16 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import StationSelector from './components/StationSelector';
 import RiskDisplay from './components/RiskDisplay';
 import MeasuresPanel from './components/MeasuresPanel';
 import AlertBanner from './components/AlertBanner';
+import HistoricalLog from './components/HistoricalLog'; // ADD THIS
 import { CircularProgress, Alert } from '@mui/material';
 
 const darkTheme = createTheme({
@@ -76,6 +77,13 @@ function App() {
         <RiskDisplay level={riskLevel} value={data?.wbgt || 29} />
         
         <MeasuresPanel riskLevel={riskLevel} />
+        
+        {/* ADD HISTORICAL LOG COMPONENT */}
+        <HistoricalLog 
+          currentWBGT={data?.wbgt}
+          currentStation={data?.station}
+          riskLevel={riskLevel}
+        />
       </Container>
     </ThemeProvider>
   );
